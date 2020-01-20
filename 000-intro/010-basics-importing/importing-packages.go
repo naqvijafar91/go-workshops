dbcfe297c8084ab068cd348c98657ae3c35f26c2
naqvijafar91 <naqvi.jafar91@gmail.com>
2020-01-20 12:48:38 +0530

    fixed import path

--- 000-intro/010-basics-importing/importing-packages.go
+++ 000-intro/010-basics-importing/importing-packages.go
@@ -5,13 +5,13 @@ import (
 
 	// if this code will be pushed to github everyone
 	// can import in that way:
-	"github.com/naqvijafar91/go-workshops/010-basics-importing/sub"
+	"github.com/naqvijafar91/go-workshops/000-intro/010-basics-importing/sub"
 	// "./sub" will work too but it's not idiomatic in GO
 )
 
 // now we can use some data from our `sub` package
 func main() {
-	fmt.Println("sub.FirstConstant:\t", sub.FirstContant)
-	fmt.Println("sub.Hoł() func call:\t", sub.Hoł())
+	fmt.Println("sub.FirstConstant:\t", sub.FirstConstant)
+	fmt.Println("sub.Hot() func call:\t", sub.Hot())
 	fmt.Println("sub.SecondConstant:\t", sub.SecondConstant)
 }
